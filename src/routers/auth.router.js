f113a660b8aae39da454c847dfe3b7d0a5912b76
devCharles <charlesipn@gmail.com>
2024-01-17 18:01:42 -0600

    auth router

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -2,8 +2,42 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/login", async (req, res) => {});
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-router.post("/signup", async (req, res) => {});
+    const token = await auth.login(email, password);
+
+    res.json({
+      success: true,
+      message: "logged in",
+      data: { token },
+    });
+  } catch (error) {
+    res.status(error.status || 500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    const userData = req.body;
+
+    const newUser = await auth.signup(userData);
+
+    res.json({
+      success: true,
+      message: "user created",
+      data: { user: newUser },
+    });
+  } catch (error) {
+    res.status(error.status || 500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 module.exports = router;
